a7b1de569d03c3fd0fcb3f0d58b07097ab5abe58
MICHAELORTIZ29 <michaelosielo@gmail.com>
2026-07-22 19:15:58 -0500

    Separar carpeta pública para cPanel

--- public/index.php
+++ public/index.php
@@ -5,16 +5,48 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+/*
+|--------------------------------------------------------------------------
+| Localizar la aplicación Laravel
+|--------------------------------------------------------------------------
+|
+| En local, Laravel está un nivel arriba de public.
+| En cPanel, la carpeta pública está separada y la aplicación está
+| ubicada en /home/miorpaco/miorpa-repo.
+|
+*/
+
+$basePath = dirname(__DIR__);
+
+if (! is_file($basePath.'/vendor/autoload.php')) {
+    $basePath = dirname(__DIR__).'/miorpa-repo';
+}
+
+/*
+|--------------------------------------------------------------------------
+| Comprobar modo mantenimiento
+|--------------------------------------------------------------------------
+*/
+
+if (file_exists($maintenance = $basePath.'/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+/*
+|--------------------------------------------------------------------------
+| Registrar Composer
+|--------------------------------------------------------------------------
+*/
 
-// Bootstrap Laravel and handle the request...
+require $basePath.'/vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Iniciar Laravel
+|--------------------------------------------------------------------------
+*/
+
 /** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once $basePath.'/bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
